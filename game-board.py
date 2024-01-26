f65d59bf98bf659c137a697bdd937e5203ff31cd
eziraa <ezratgab@gmail.com>
2024-01-27 01:17:03 +0300

    the board of the game started

---
+++ game-board.py
@@ -0,0 +1,20 @@
+# Importing important  packages
+from turtle import Turtle , Screen
+import random
+import time
+
+# Declaring global object
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+tetris_compnnts_list = []
+
+
+class GameBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.screen = Screen()
+        self.screen.tracer(0)
+        self.screen.setup(500, 600)
+        self.screen.bgcolor("black")
+        self.screen.listen()
+
