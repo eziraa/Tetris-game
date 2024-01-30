68df5de629d3092301bb7e700f180b36810b3571
eziraa <ezratgab@gmail.com>
2024-01-31 00:16:36 +0300

    create mathod in class TBlock implemeted

--- t_block.py
+++ t_block.py
@@ -7,3 +7,13 @@ POSITION_LIST = [(-20, 270), (2, 270), (24, 270), (2, 292)]
 class TBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
+        self.create()
+
+    def create(self):
+        for position in POSITION_LIST:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.color("orange")
+            new_segment.speed("fastest")
+            new_segment.goto(position)
+            self.tetris_block_segments.append(new_segment)
