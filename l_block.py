9e01722199feb0683dd91d6e0b340565d875ab5d
eziraa <ezratgab@gmail.com>
2024-01-31 00:04:31 +0300

    create method in LBlock imlemented

--- l_block.py
+++ l_block.py
@@ -2,6 +2,20 @@ from turtle import Turtle
 from tetris_block import TetrisBlock
 
 
+# to indicate the position of individual segment of the block
+
+POSITION_LIST = [(-20, 270), (2, 270), (2, 292), (-20, 292)]
+
 class LBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
+        self.create()
+
+    def create(self):
+        for position in POSITION_LIST:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.color("blue")
+            new_segment.speed("fastest")
+            new_segment.goto(position)
+            self.tetris_block_segments.append(new_segment)
