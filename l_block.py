17367d1c9bc48c383ad164629db6a998c13054cc
eziraa <ezratgab@gmail.com>
2024-01-31 00:19:46 +0300

    create mothod in LBlock class implmeted

--- l_block.py
+++ l_block.py
@@ -7,3 +7,14 @@ POSITION_LIST = [(24, 270), (2, 270), (2, 292), (2, 314)]
 class LBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
+        self.create()
+
+    def create(self):
+        for position in POSITION_LIST:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.color("yellow")
+            new_segment.speed("fastest")
+            new_segment.goto(position)
+            self.tetris_block_segments.append(new_segment)
+
