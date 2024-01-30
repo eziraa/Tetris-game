5b3b930efff63493c839fe1bd04cb68fed60a1c9
eziraa <ezratgab@gmail.com>
2024-01-31 00:18:17 +0300

    file lblock modifed

--- l_block.py
+++ l_block.py
@@ -1,21 +1,9 @@
-from turtle import Turtle
 from tetris_block import TetrisBlock
+from turtle import Turtle
 
+POSITION_LIST = [(24, 270), (2, 270), (2, 292), (2, 314)]
 
-# to indicate the position of individual segment of the block
-
-POSITION_LIST = [(-20, 270), (-20, 292), (2, 292), (2, 314)]
 
 class LBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
-        self.create()
-
-    def create(self):
-        for position in POSITION_LIST:
-            new_segment = Turtle("square")
-            new_segment.penup()
-            new_segment.color("blue")
-            new_segment.speed("fastest")
-            new_segment.goto(position)
-            self.tetris_block_segments.append(new_segment)
