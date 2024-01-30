92156760fb12b1b593c4f6abb3917cf225b8d756
eziraa <ezratgab@gmail.com>
2024-01-31 00:36:33 +0300

    create method in class ZBlock created

--- z_block.py
+++ z_block.py
@@ -1,7 +1,19 @@
 from turtle import Turtle
 from tetris_block import TetrisBlock
 
+POSITION_LIST = [(24, 270), (24, 292), (2, 292), (2, 314)]
+
 
 class ZBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
+        self.create()
+
+    def create(self):
+        for position in POSITION_LIST:
+            new_segment = Turtle('square')
+            new_segment.penup()
+            new_segment.color('green')
+            new_segment.speed("fastest")
+            new_segment.goto(position)
+            self.tetris_block_segments.append(new_segment)
