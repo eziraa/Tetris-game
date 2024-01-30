06365a3b9aca8f78fb7b012a2f7d865e003faf1f
eziraa <ezratgab@gmail.com>
2024-01-31 02:27:18 +0300

    set_up method implmented to make the block fully functional

--- j_block.py
+++ j_block.py
@@ -9,6 +9,7 @@ class JBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
         self.create()
+        self.set_up()
 
     def create(self):
         for position in POSITION_LIST:
@@ -18,3 +19,11 @@ class JBlock(TetrisBlock):
             new_segment.speed("fastest")
             new_segment.goto(position)
             self.tetris_block_segments.append(new_segment)
+
+    def set_up(self):
+        self.front.extend([self.tetris_block_segments[0], self.tetris_block_segments[1]])
+        self.head.extend([self.tetris_block_segments[0], self.tetris_block_segments[3]])
+        self.left.extend([self.tetris_block_segments[0], self.tetris_block_segments[2], self.tetris_block_segments[3]])
+        self.right.extend([self.tetris_block_segments[3], self.tetris_block_segments[2], self.tetris_block_segments[1]])
+        for segment in self.tetris_block_segments:
+            segment.setheading(270)
