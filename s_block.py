ffeb2c0dfe05b1506598dfcfbd7de286cb13ac73
eziraa <ezratgab@gmail.com>
2024-02-01 17:16:49 +0300

    implmenting setup method in SBlock class  to diferenciate front, head, left and right

--- s_block.py
+++ s_block.py
@@ -12,6 +12,8 @@ class SBlock(TetrisBlock):
         self.create()
 
     def create(self):
+        """
+        This method is to create segments of SBlock"""
         for position in POSITION_LIST:
             new_segment = Turtle('square')
             new_segment.penup()
@@ -19,3 +21,12 @@ class SBlock(TetrisBlock):
             new_segment.speed("fastest")
             new_segment.goto(position)
             self.tetris_block_segments.append(new_segment)
+    def set_up(self):
+        """
+        This method is to assign left, head, front, right segment of LBlock"""
+        self.front.extend([self.tetris_list[2], self.tetris_list[0]])
+        self.left.extend([self.tetris_list[0], self.tetris_list[1], self.tetris_list[3]])
+        self.right.extend([self.tetris_list[0], self.tetris_list[2], self.tetris_list[3]])
+        self.head.extend([self.tetris_list[3], self.tetris_list[1]])
+        for segment in self.tetris_list:
+            segment.setheading(270)
