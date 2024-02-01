ea256c5b9842f66df0f3e5dc996e5a316c2e7116
eziraa <ezratgab@gmail.com>
2024-02-01 18:35:56 +0300

    file one_block renamed to i_block and Class OneBlock renamed to IBlock for clarity

---
+++ i_block.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+from tetris_block import TetrisBlock
+
+# To indicating the correct position of the individual segments of this block
+POSITION_LIST = [(2, 270), (2, 292), (2, 314), (2, 336)]
+
+
+# Creating class OneBlock
+class IBlock(TetrisBlock):
+    def __init__(self):
+        super().__init__()
+        self.create()
+        self.set_up()
+
+    # Implementing create method to create ui of the block
+    def create(self):
+        """
+        This method is to create segments of I"""
+        for position in POSITION_LIST:
+            new_segment = Turtle("square")
+            new_segment.penup()
+            new_segment.color('red')
+            new_segment.speed("fastest")
+            new_segment.goto(position)
+            self.tetris_block_segments.append(new_segment)
