b86f2a0095a224262a3b871e378c03a3f33ed000
eziraa <ezratgab@gmail.com>
2024-02-01 13:27:05 +0300

    Abstract method removed for simplicity

--- tetris_block.py
+++ tetris_block.py
@@ -1,9 +1,10 @@
 import time
+
 from abc import ABC, abstractmethod
 from turtle import Turtle
 
 
-class TetrisBlock(Turtle, ABC):
+class TetrisBlock(Turtle):
     def __init__(self):
         super().__init__()
         self.tetris_block_segments = []
@@ -12,32 +13,6 @@ class TetrisBlock(Turtle, ABC):
         self.head = []
         self.right = []
         self.left = []
-
-    # Abstract methods
-
-    @abstractmethod
-    def create(self):
-        pass
-
-    @abstractmethod
-    def set_up(self):
-        pass
-
-    @abstractmethod
-    def update_set_left(self):
-        pass
-
-    @abstractmethod
-    def update_set_right(self):
-        pass
-
-    @abstractmethod
-    def update_set_down(self):
-        pass
-
-    @abstractmethod
-    def update_set_up(self):
-        pass
 
     def startMove(self):
         for segment in self.tetris_block_segments:
