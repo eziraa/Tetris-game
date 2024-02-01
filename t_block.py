858a948f654a861994300328519a5fda33d405b2
eziraa <ezratgab@gmail.com>
2024-02-01 17:20:28 +0300

    implmenting setup method in TBlock class  to diferenciate front, head, left and right

--- t_block.py
+++ t_block.py
@@ -8,8 +8,11 @@ class TBlock(TetrisBlock):
     def __init__(self):
         super().__init__()
         self.create()
+        self.set_up()
 
     def create(self):
+        """
+        This method is to create segment of TBlock """
         for position in POSITION_LIST:
             new_segment = Turtle("square")
             new_segment.penup()
@@ -17,3 +20,13 @@ class TBlock(TetrisBlock):
             new_segment.speed("fastest")
             new_segment.goto(position)
             self.tetris_block_segments.append(new_segment)
+            
+    def set_up(self):
+        """
+        This method is to seprate head, front, left and right of TBlock """
+        self.front.extend([self.tetris_list[0], self.tetris_list[1], self.tetris_list[2]])
+        self.left.extend([self.tetris_list[0], self.tetris_list[3]])
+        self.head.extend([self.tetris_list[0], self.tetris_list[2], self.tetris_list[3]])
+        self.right.extend([self.tetris_list[3], self.tetris_list[2]])
+        for segment in self.tetris_list:
+            segment.setheading(270)
