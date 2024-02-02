9dc59cc8bb9b997cb097a269bc11554c3937ca96
eziraa <ezratgab@gmail.com>
2024-02-02 08:46:28 +0300

    updating rotate method in class TetrisBlock to set the head, front, right and left of the block when it rotate

--- tetris_block.py
+++ tetris_block.py
@@ -12,6 +12,7 @@ class TetrisBlock(Turtle):
         self.head = []
         self.right = []
         self.left = []
+        self.center = 0
         self.rotate_index = 0
         self.rotate_position = []
 
@@ -23,8 +24,14 @@ class TetrisBlock(Turtle):
         position = self.rotate_position[self.rotate_index]()
         for i in range(len(position)):
             self.tetris_block_segments[i].goto(position[i])
-            self.front.clear()
-            self.front.extend(self.right if self.rotate_index == 0 else self.head if self.rotate_index == 1 else self.left)
-            self.rotate_index = self.rotate_index + 1 if self.rotate_index <= 2 else 0
+        front = self.front
+        self.front.clear()
+        self.front.extend(self.right)
+        self.right.clear()
+        self.right.extend(self.head)
+        self.head.clear()
+        self.head.extend(self.left)
+        self.left.clear()
+        self.left.extend(front)
 
         
