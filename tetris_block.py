fbb3e5bfdb4183eadf08f01d12f6573e4cc077ae
eziraa <ezratgab@gmail.com>
2024-02-02 08:04:10 +0300

    rotate methos add to rotate the block

--- tetris_block.py
+++ tetris_block.py
@@ -1,6 +1,5 @@
 import time
 
-from abc import ABC, abstractmethod
 from turtle import Turtle
 
 
@@ -13,7 +12,19 @@ class TetrisBlock(Turtle):
         self.head = []
         self.right = []
         self.left = []
+        self.rotate_index = 0
+        self.rotate_position = []
 
     def startMove(self):
         for segment in self.tetris_block_segments:
             segment.forward(22)
+
+    def rotate(self):
+        position = self.rotate_position[self.rotate_index]()
+        for i in range(len(position)):
+            self.tetris_block_segments[i].goto(position[i])
+            self.front.clear()
+            self.front.extend(self.right if self.rotate_index == 0 else self.head if self.rotate_index == 1 else self.left)
+            self.rotate_index = self.rotate_index + 1 if self.rotate_index <= 2 else 0
+
+        
