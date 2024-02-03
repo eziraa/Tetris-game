8849c2c1a9e53c1c9d3429da14ebb92462913836
eziraa <ezratgab@gmail.com>
2024-02-03 14:05:19 +0300

    creating method to display the updated score on the game board

--- score_board.py
+++ score_board.py
@@ -16,15 +16,20 @@ class ScoreBoard(Turtle):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
         self.score_update_level = 0
+        self.set_up()
 
-
-def set_up(self):
-    while True:
-        text = self.screen.textinput("Choose the level of the game",
-                                         "1. level 1 \n2. level 2 \n3. level 3 \n4. level 4 \n5. level 5 \n6. level 6 \n7. level 7")
-        if text is None:
-            exit()
-        elif text.isdigit() and 0 < int(text) <= 7:
-            self.fast_level = 0.7 if text == '1' else 0.6 if text == '2' else 0.5 if text == '3' else 0.4 if text == '4' else 0.3 if text == '5' else 0.2 if text == '6' else 0.1
-            self.score_update_level = 4 if text == '1' else 5 if text == '2' else 6 if text == '3' else 7 if text == '4' else 8 if text == '5' else 9 if text == '6' else 10
-            break
+    def set_up(self):
+        while True:
+            text = self.screen.textinput("Choose the level of the game",
+                                            "1. level 1 \n2. level 2 \n3. level 3 \n4. level 4 \n5. level 5 \n6. level 6 \n7. level 7")
+            if text is None:
+                exit()
+            elif text.isdigit() and 0 < int(text) <= 7:
+                self.fast_level = 0.7 if text == '1' else 0.6 if text == '2' else 0.5 if text == '3' else 0.4 if text == '4' else 0.3 if text == '5' else 0.2 if text == '6' else 0.1
+                self.score_update_level = 4 if text == '1' else 5 if text == '2' else 6 if text == '3' else 7 if text == '4' else 8 if text == '5' else 9 if text == '6' else 10
+                break
+            
+    def update_score(self):
+        self.score += self.score_update_level
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
